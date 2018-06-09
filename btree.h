ac97ebba89777113bb80ebf5c49c5f6f11871a9a
Kálley Wilkerson <yellakkalley@hotmail.com>
2018-06-09 15:11:54 -0300

    Btree inicializada

---
+++ btree.h
@@ -0,0 +1,17 @@
+#ifndef __BTREE__H_
+#define __BTREE__H_
+
+typedef struct nob{
+  char chaves**;
+  int NRR*;
+  int folha;
+  int n_nos;
+  struct nob**;
+} NoB;
+
+typedef struct barv{
+  NoB* raiz;
+  int ordem;
+} Barv;
+
+#endif
