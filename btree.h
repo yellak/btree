08c8c70b22a32bc57fca9c9c61c8f3735029695b
Felipe Lima <felipesoueu@live.com>
2018-06-09 17:29:42 -0300

    Funções colocadas na .h

--- btree.h
+++ btree.h
@@ -16,4 +16,8 @@ typedef struct barv{
   int ordem;
 } Barv;
 
+void insere_arvore_naocheia(NoB* no, char* chave, int NRR, int ordem);
+
+void inserir_btree(Barv* arv, char* chave, int NRR);
+
 #endif
